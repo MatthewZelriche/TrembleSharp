a5ca5232f22150b8886a9ded8de6a9dfbede8e18
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-12-12 18:53:50 -0600

    Add Build script for handling Tremble Dependency

---
+++ build.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import argparse
+import subprocess
+import os
+import platform
+import shutil
+
+parser = argparse.ArgumentParser(description='Build Tremble.')
+parser.add_argument('--config', dest='buildConfig',
+                   default="Release",
+                   help='The build type to use. \'Debug\' or \'Release\'')
+parser.add_argument('--jobs', dest='jobs',
+                   default="1",
+                   help='The number of jobs to pass to the build system')
+
+args = parser.parse_args()
+
+subprocess.run(["cmake", "-Svendor/Tremble/", "-Bbuild/"])
+subprocess.run(["cmake", "--build", "build", "--config", args.buildConfig, "-j" + args.jobs])
+subprocess.run(["dotnet", "build", "src/src.csproj", "--output", "build/" + args.buildConfig])
--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+   private static void Main()
+   {
+      Console.WriteLine("Hello!");
+   }
+}
